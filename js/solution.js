440df3d8fedd119f49f209b2428722fb21d13ca9
schedrov <rocolusso@icloud.com>
2022-05-31 13:37:30 +0300

    rewrite2

--- js/solution.js
+++ js/solution.js
@@ -1,20 +1,26 @@
 'use strict';
-
 /*
 Написать функцию createTable(), которая выводит таблицу 10 × 10, заполненную числами от 1 до 100.
 
 P.S. Подумайте о том как лучше обновлять DOM структуру, от этого зависит производительность скрипта.
  */
-
 void function (){
     const createTable = () => {
         const table = document.createElement('table');
             for (let i = 1; i <= 10; i++ ) {
                 const tr = document.createElement('tr');
-                    for (let k = (i - 1) + '1' ; k <= i * 10 ; k++) {
-                        const td = document.createElement('td');
-                        td.innerHTML = `${k}`;
-                        tr.append(td);
+                    if (i === 1){
+                        for (let k = 1; k <= 10; k++) {
+                            const td = document.createElement('td');
+                            td.innerHTML = `${k}`;
+                            tr.append(td);
+                        }
+                    } else {
+                        for (let k = (i - 1) + '1' ; k <= i * 10 ; k++) {
+                            const td = document.createElement('td');
+                            td.innerHTML = `${k}`;
+                            tr.append(td);
+                        }
                     }
                 table.append(tr);
             }
